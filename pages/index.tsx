1533c9b044dda2fa5175d8f4846cff536a3fbbd7
tom <dev@tomblanc.fr>
2022-11-03 10:14:33 +0100

    Creating Writing machine effect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,36 @@
 import Head from "next/head";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Consolas from "../components/consolas/consolas";
 
+function useConsole(text: string) {
+  const [cnsl, setConsole] = useState("");
+  useEffect(() => {
+    console.log(text);
+    let timeout: Array<NodeJS.Timeout> = [];
+    for (let i = 0; i < text.length; i++) {
+      timeout.push(
+        setTimeout(() => {
+          setConsole((prev) => prev + text[i]);
+        }, 100 * i)
+      );
+    }
+    return () => {
+      timeout.forEach((t) => clearTimeout(t));
+    };
+  }, []);
+  return cnsl;
+}
+
 export default function Home() {
+  let cnsl = useConsole("Welcome in the next world");
+
   return (
     <div className="h-full w-full flex flex-col items-center justify-center text-white p-2">
-      <h1 className="text-4xl">Welcome in the next world</h1>
+      <h1 className="text-4xl">
+        {"> "}
+        {cnsl} <b className="animate-ping"> |</b>
+      </h1>
     </div>
   );
 }
