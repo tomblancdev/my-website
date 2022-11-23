e402380707cf9878f0da8dea83fe8c61f225d8fd
tom <dev@tomblanc.fr>
2022-11-23 09:11:51 +0100

    about page css changes

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
-    <div className=" h-full p-0 m-0 flex flex-col justify-center items-center font-mono bg-gradient-to-tl from-[#001337] to-[#021e51]">
-      <div className=" h-full p-0 m-0 flex flex-col justify-center items-center font-mono bg-gradient-to-tl from-[#001337] to-[#021e51]">
+    <div className=" h-full p-0 m-0 flex flex-col justify-center items-center">
+      <div className=" h-full p-0 m-0 flex flex-col justify-center items-center">
         <h1 className="text-white text-5xl">About</h1>
       </div>
     </div>
