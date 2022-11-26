591f66a88a85ed7bb2dbabb1474a629492251170
tom <dev@tomblanc.fr>
2022-11-26 10:15:25 +0100

    Reworking on types and props

--- components/consolas/command.tsx
+++ components/consolas/command.tsx
@@ -1,18 +1,32 @@
 import Router, { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useKeyDown } from "../hooks/keyEvents";
-import { ErrorResult, HistoryItem, SuccessResult } from "./utils/commands";
+import {
+  CommandConfig,
+  CommandField,
+  ErrorResult,
+  HistoryItem,
+  SuccessResult,
+} from "./utils/commands";
 
-export default function Command(props: { [key: string]: any }) {
+interface propsInterface {
+  toWrite: string;
+  commandList: { [key: string]: CommandConfig };
+  addToHistory: (history: HistoryItem) => void;
+}
+
+export default function Command({
+  toWrite,
+  commandList,
+  addToHistory,
+}: propsInterface) {
   // setting the command state
   let [command, setCommand] = useState("");
   // listening on keydown
   let keyPressed = useKeyDown();
-  // creating empy toWrite wich is the character wich can be written on keyDown
-  let toWrite = "";
   // if there is a toWrite in props, set it to toWrite esle set the default one
-  props.toWrite
-    ? (toWrite = props.toWrite)
+  toWrite
+    ? (toWrite = toWrite)
     : (toWrite =
         "abcdefghijklmnopqrstuvwxyzAZERTYUIOPQSDFGHJKLMWXCVBN0123456789- "); //default toWrite
 
@@ -29,14 +43,14 @@ export default function Command(props: { [key: string]: any }) {
           "A problem occured while executing command"
         );
         // if the key pressed is enter, execute the command
-        if (command.split(" ")[0] in props.commandList) {
+        if (command.split(" ")[0] in commandList) {
           // if the command is in the commandList
           // get the command
-          let cmd = command.split(" ")[0] as keyof typeof props.commandList;
+          let cmd = command.split(" ")[0] as keyof typeof commandList;
           try {
             // try to execute the command
             // get the result of the command i a temporary result variable
-            let tempResult = props.commandList[cmd].execute(
+            let tempResult = commandList[cmd].execute(
               command.split(" ").slice(1)
             );
             // if result does not exist create empty success result
@@ -54,8 +68,7 @@ export default function Command(props: { [key: string]: any }) {
           result = new ErrorResult("command not found");
         }
         // add the historyItem to history if command is passed
-        props.addToHistory &&
-          props.addToHistory(new HistoryItem(command, result));
+        addToHistory && addToHistory(new HistoryItem(command, result));
         // reset the command
         setCommand("");
       }
